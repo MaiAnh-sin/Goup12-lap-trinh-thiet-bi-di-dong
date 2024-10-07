6967dec5727ab41b245fb2c79c956040ca376151
22010242@st.phenikaa-uni.edu.vn <hungdam969@gmail.com>
2024-10-07 17:56:53 +0700

    test lai cm

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -55,7 +55,6 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: widgetList[_myIndex],
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
           setState(() {
