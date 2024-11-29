1c8572278914cab66caac330c0079c03de839342
22010242@st.phenikaa-uni.edu.vn <hungdam969@gmail.com>
2024-11-30 03:56:46 +0700

    up date fire base

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:mobile_app/UI/Login/login_page.dart';
 import 'package:mobile_app/consts.dart';
 
-void main() {
+void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
   Gemini.init(apiKey: GEMINI_API_KEY);
   runApp(const MyApp());
 }
